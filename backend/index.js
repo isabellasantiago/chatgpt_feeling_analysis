62b07b2b376a0b1b052fae6063db2e144996c78b
Rodrigo Bossini <professorbossini@gmail.com>
2023-05-19 21:24:47 -0300

    chatgpt-rn: dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 //aqui aplicamos um middleware
 app.use(express.json())
+
+// const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+// const PORT = process.env.PORT
+
+const { OPENAI_API_KEY, PORT } = process.env
 
 // GET localhost:4000/hello -> {"mensagem": "Hello direto do Back End"}
 app.get('/hello', (req, res) => {
@@ -11,6 +17,6 @@ app.get('/hello', (req, res) => {
 // POST localhost:4000/sentimentos ({"texto": "Estou feliz"}) ->  {"sentimento": "Positivo" }
 
 
-const porta = 4000
+const porta = PORT || 4000
 
 app.listen(porta, () => console.log (`Servidor OK. Porta ${porta}`))
